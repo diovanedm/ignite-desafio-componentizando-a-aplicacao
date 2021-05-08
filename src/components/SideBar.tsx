49d85486015693a14b657e3495f97a65f1d97f8c
Diovane Maia <diovane.dm@gmail.com>
2021-05-08 12:50:56 -0300

    Aplicação concluída

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
+import { Button } from '../components/Button';
+
+
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+interface SideBarPros{ 
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClickButton: Function
+}
+
+export function SideBar({genres, selectedGenreId, handleClickButton}: SideBarPros) {
   // Complete aqui
+
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+  )
 }
